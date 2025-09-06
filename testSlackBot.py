f614733e32bdd3645f92fc0f3b37a1d1dd90e1d1
revathi2592 <115491785+revathi2592@users.noreply.github.com>
2025-09-06 11:05:22 +0530

    Update testSlackBot.py

--- testSlackBot.py
+++ testSlackBot.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
  
 app = Flask(__name__)
- 
+
+
 @app.route('/slack/events', methods=['POST'])
 def slack_events():
     data = request.get_json()
@@ -14,4 +15,5 @@ def slack_events():
     return '', 200
  
 if __name__ == '__main__':
+
     app.run(port=3000)
